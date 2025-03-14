cca35d38f677277f29681e197b7c1ead3ba14c8b
Pavel Chernous <goltshteyngottorp@gmail.com>
2025-03-14 16:58:20 +0300

    Fix: remove bags from functions

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,4 +1,6 @@
 import datetime
+
+import pytz
 from flask import Flask, jsonify
 import random
 import re
@@ -10,18 +12,21 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 
-cars = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+cars_brands = ['Chevrolet', 'Renault', 'Ford', 'Lada']
 @app.route('/cars')
-def cars():
-    return cars
+def print_cars():
+    return ', '.join(cars_brands)
 
 cat_breeds  = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
-random_cat = random.choice(cat_breeds)
 @app.route('/cats')
 def get_random_cat():
+    random_cat = random.choice(cat_breeds)
     return random_cat
 
-current_time = datetime.datetime.now().utcnow()
+utc_0 = datetime.datetime.now().utcnow()
+utc_plus_3 = pytz.timezone('Europe/Moscow')  # Например, Московское время
+# Переводим текущее время в UTC+3
+current_time = utc_0.replace(tzinfo=pytz.utc).astimezone(utc_plus_3)
 @app.route('/get_time/now')
 def get_time_now():
     return f'Точное время: {current_time}'
