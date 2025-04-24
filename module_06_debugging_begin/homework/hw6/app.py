5d758bbe540b91588c6db6c670dac9400f6fbe86
Pavel Chernous <goltshteyngottorp@gmail.com>
2025-04-25 00:49:36 +0300

    Task 6: add custom 404 handler and back buttons to /dogs, /cats, and /cats/<id> pages

--- module_06_debugging_begin/homework/hw6/app.py
+++ module_06_debugging_begin/homework/hw6/app.py
@@ -6,29 +6,57 @@
 список всех доступных страниц на сайте с возможностью перехода на них.
 """
 
-from flask import Flask
-
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/dogs')
 def dogs():
-    return 'Страница с пёсиками'
+    return f'''
+        <h1>Страница с пёсиками<h1>
+        <button onclick="window.history.back()">Вернуться назад</button>
+    '''
 
 
 @app.route('/cats')
 def cats():
-    return 'Страница с котиками'
+    return '''
+        <h1>Страница с котиками</h1>
+        <button onclick="window.history.back()">Вернуться назад</button>
+    '''
 
 
 @app.route('/cats/<int:cat_id>')
 def cat_page(cat_id: int):
-    return f'Страница с котиком {cat_id}'
+    return f'''
+        <h1>Страница с котиком {cat_id}</h1>
+        <button onclick="window.history.back()">Вернуться назад</button>
+    '''
 
 
 @app.route('/index')
 def index():
-    return 'Главная страница'
+    return f'''
+        <h1>Главная страница</h1>
+        <button onclick="window.history.back()">Вернуться назад</button>
+    '''
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    routes = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods:
+            defaults = {arg: 1 for arg in rule.arguments}
+            try:
+                url = url_for(rule.endpoint, **defaults)
+                routes.append((rule.endpoint, url))
+            except Exception:
+                pass
+
+    routes.sort(key=lambda x: x[0])
+
+    return render_template('404_error.html', routes=routes), 404
 
 
 if __name__ == '__main__':
