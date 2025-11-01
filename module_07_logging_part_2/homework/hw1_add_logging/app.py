cb9d918ee52aa3d457824caeb026b63a5f164222
Chernous Pavel <goltshteyngottorp@gmail.com>
2025-11-01 00:44:49 +0000

    Add logger for app

--- module_07_logging_part_2/homework/hw1_add_logging/app.py
+++ module_07_logging_part_2/homework/hw1_add_logging/app.py
@@ -1,8 +1,15 @@
 import sys
+import logging
 from utils import string_to_operator
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
 
 
 def calc(args):
+    # Получаем входные данные
+    logger.debug(f"Получаем аргументы: {args}")
+
     print("Arguments: ", args)
 
     num_1 = args[0]
@@ -11,24 +18,35 @@ def calc(args):
 
     try:
         num_1 = float(num_1)
+        # Получаем первое значение
+        logger.debug(f"Пробуем преобразовать первое значение: {num_1}")
     except ValueError as e:
+        logger.error(f"Ошибка преобразования первого значения: {num_1}")
         print("Error while converting number 1")
         print(e)
 
     try:
         num_2 = float(num_2)
+        # Получаем второе значение
+        logger.debug(f"Пробуем преобразовать второе значение: {num_2}")
     except ValueError as e:
+        logger.error(f"Ошибка преобразования второго значения: {num_2}")
         print("Error while converting number 2")
         print(e)
 
     operator_func = string_to_operator(operator)
+    logger.debug(f"Получаем функцию оператора: {operator_func}")
 
     result = operator_func(num_1, num_2)
+    logger.debug(f"Выполнение операции: {num_1} {operator} {num_2} = {result}")
 
     print("Result: ", result)
     print(f"{num_1} {operator} {num_2} = {result}")
+    logger.info(f"Успешно выполнена операция: {num_1} {operator} {num_2} = {result}")
 
 
 if __name__ == '__main__':
+    logger.info(f"Запуск приложения!")
     # calc(sys.argv[1:])
     calc('2+3')
+    logger.info(f"Остановка работы приложения!")
