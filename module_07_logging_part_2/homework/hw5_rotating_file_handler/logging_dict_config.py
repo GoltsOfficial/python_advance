cba2983a25884889bd3225465e422ac9829d9716
Chernous Pavel <goltshteyngottorp@gmail.com>
2025-11-01 02:12:53 +0000

    Fix file configure

--- module_07_logging_part_2/homework/hw5_rotating_file_handler/logging_dict_config.py
+++ module_07_logging_part_2/homework/hw5_rotating_file_handler/logging_dict_config.py
@@ -1,3 +1,4 @@
+# logging_dict_config.py
 LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -45,30 +46,22 @@ LOGGING_CONFIG = {
             'level': 'CRITICAL',
             'formatter': 'standard',
             'filename': 'calc_critical.log'
+        },
+        'utils_file': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'standard',
+            'filename': 'utils.log',
+            'when': 'H',
+            'interval': 10,
+            'backupCount': 1,
         }
-    },
-
-    'utils_file': {
-                '()': 'logging.handlers.TimedRotatingFileHandler',
-                'level': 'INFO',
-                'formatter': 'standard',
-                'filename': 'utils.log',
-                'when': 'H',
-                'interval': 10,
-                'backupCount': 1,
     },
 
     'loggers': {
         '': {
             'level': 'DEBUG',
-            'handlers': [
-                'stdout',
-                'debug_file',
-                'info_file',
-                'warning_file',
-                'error_file',
-                'critical_file'
-            ]
+            'handlers': ['stdout', 'debug_file', 'info_file', 'warning_file', 'error_file', 'critical_file']
         },
         'utils': {
             'level': 'INFO',
