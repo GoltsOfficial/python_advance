b16db375cfce1c876ef6706c58f3b6922c507daf
Chernous Pavel <goltshteyngottorp@gmail.com>
2025-11-03 21:21:49 +0000

    Save fixes

--- module_08_deploy/materials/new_year_application/app.py
+++ module_08_deploy/materials/new_year_application/app.py
@@ -1,36 +1,20 @@
 import os
-
 from flask import Flask, render_template, send_from_directory
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
-template_folder = os.path.join(root_dir, "templates")
-js_directory = os.path.join(template_folder, "js")
-css_directory = os.path.join(template_folder, "css")
-images_directory = os.path.join(template_folder, "images")
+app = Flask(__name__,
+            template_folder=os.path.join(root_dir, "templates"))
 
-app = Flask(__name__, template_folder=template_folder)
-
+# Самореализованный endpoint для статики
+@app.route("/static/<path:filename>")
+def send_static(filename):
+    static_folder = os.path.join(root_dir, "static")
+    return send_from_directory(static_folder, filename)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route("/js/<path:path>")
-def send_js(path):
-    return send_from_directory(js_directory, path)
-
-
-@app.route("/css/<path:path>")
-def send_css(path):
-    return send_from_directory(css_directory, path)
-
-
-@app.route("/images/<path:path>")
-def send_images(path):
-    return send_from_directory(images_directory, path)
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=8000, debug=True)
